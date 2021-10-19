fd48d924fbfcd1e25ea6d8be1b575d71a8a08889
Keaton Rohwer <krquin@gmail.com>
2021-10-18 19:00:38 -0500

    vectorUtils is deprecated pogFish & readme updates

--- tests/vectorTest.cpp
+++ tests/vectorTest.cpp
@@ -1,19 +1,20 @@
-#include "Vector.h"
+#include <glm/glm.hpp>
+
 #include <math.h>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	Vector v1 = Vector::Vector(2, 3);
-	Vector v2 = Vector::Vector(5, 1);
+	glm::vec3 v1(2.0, 3.0, 0.0);
+	glm::vec3 v2(5.0, 1.0, 0.0);
 	
 	cout << "Your Vectors:" << endl;
-	cout << v1.x << ", " << v1.y << " mag: " << v1.Vector::magnitude() << endl;
-	cout << v2.x << ", " << v2.y << " mag: " << v2.Vector::magnitude() << endl;
+	cout << v1.x << ", " << v1.y << " mag: " << glm::length(v1) << endl;
+	cout << v2.x << ", " << v2.y << " mag: " << glm::length(v2) << endl;
 
-	Vector v3 = add(v1, v2);
-	cout << v3.x << ", " << v3.y << " mag: " << v3.Vector::magnitude() << endl;
+	glm::vec3 v3 = v1 + v2;
+	cout << v3.x << ", " << v3.y << " mag: " << glm::length(v3) << endl;
 	getchar();
 
 
