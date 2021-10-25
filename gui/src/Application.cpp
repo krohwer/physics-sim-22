96e2e1089f5316f1d85f9c21e668216a0a44f5ac
Keaton Rohwer <krquin@gmail.com>
2021-10-25 12:57:23 -0500

    Moved imgui to its own project, fixed compilation and linker errors.  Everything should work now

--- gui/src/Application.cpp
+++ gui/src/Application.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 
-#include <imgui/imgui.h>
-#include <imgui/imgui_impl_glfw.h>
-#include <imgui/imgui_impl_opengl3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 #include <GLFW/glfw3.h> // also uses glew 2.1.0 in precompiled header
 #include <glm/glm.hpp> // glm version 9.9.8
